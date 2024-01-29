25041416d5da5da1d8a2934a18b0eb3ecc660fe1
Aliaksandr <djvalentinoff@gmail.com>
2024-01-29 15:07:11 +0300

    feat: add burger

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,22 +1,55 @@
-"use client"
+"use client";
 
 import { Link } from "react-scroll";
 import Logo from "../logo";
+import { useMediaQuery } from "@/src/hooks";
+import { useState } from "react";
 import styles from "./header.module.scss";
+import stylesMenu from "./mobileMenu.module.scss"
+
 
 const Header = () => {
+  const isMobile = useMediaQuery(640);
+  const [menuOpen, setMenuOpen] = useState(false);
   const spy = true;
   const smooth = true;
   const offset = 140;
   const duration = 500;
+  const currentMenuItemClass = isMobile
+    ? stylesMenu.menu__item
+    : styles.header__nav__list__item;
+
+  const handleToggleMenu = () => {
+    (document.querySelector("body") as HTMLBodyElement).classList.toggle(
+      "overflow-hidden"
+    );
+    setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    (document.querySelector("body") as HTMLBodyElement).classList.remove(
+      "overflow-hidden"
+    );
+    setMenuOpen(false);
+  };
 
   return (
     <header className={styles.header}>
       <div className={`container ${styles.header__container}`}>
         <Logo />
-        <nav className={styles.header__nav}>
-          <ul className={styles.header__nav__list}>
-            <li className={styles.header__nav__list__item}>
+        {isMobile && (
+          <button
+            className={`${styles.burger_menu} ${menuOpen ? styles.open : ""}`}
+            onClick={handleToggleMenu}
+          >
+            <span />
+            <span />
+            <span />
+          </button>
+        )}
+        <nav className={`${isMobile ? stylesMenu.menu : styles.header__nav} ${menuOpen ? stylesMenu.open : ""}`}>
+          <ul className={`${isMobile ? styles.list_reset : styles.header__nav__list}`}>
+            <li className={currentMenuItemClass}>
               <Link
                 className={styles.header__nav__list__item__link}
                 to="about"
@@ -28,7 +61,7 @@ const Header = () => {
                 Обо мне
               </Link>
             </li>
-            <li className={styles.header__nav__list__item}>
+             <li className={currentMenuItemClass}>
               <Link
                 className={styles.header__nav__list__item__link}
                 to="about"
@@ -40,7 +73,7 @@ const Header = () => {
                 Навыки
               </Link>
             </li>
-            <li className={styles.header__nav__list__item}>
+             <li className={currentMenuItemClass}>
               <Link
                 className={styles.header__nav__list__item__link}
                 to="about"
@@ -52,7 +85,7 @@ const Header = () => {
                 Портфолио
               </Link>
             </li>
-            <li className={styles.header__nav__list__item}>
+             <li className={currentMenuItemClass}>
               <Link
                 className={styles.header__nav__list__item__link}
                 to="about"
